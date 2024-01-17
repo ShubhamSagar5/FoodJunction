93f97f5a2bfe26ee16531727799bd182d4ba7082
shubham <shubhamsagar628@gmail.com>
2024-01-17 14:36:33 +0530

    Shimmer UI implement

--- src/components/Body.js
+++ src/components/Body.js
@@ -1,17 +1,25 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState } from 'react'
 import RestaurantCard from './RestaurantCard'
 import { restaurantsInfo } from '../utilis/MockData'
+import Shimmer from './Shimmer'
 
 const Body = () => {
   
   const [resData ,setResData] = useState(restaurantsInfo)
   
 
+
+  
+
+
   const handleTopRated = () => {
     setResData(resData.filter((res)=> res.info.avgRating > 4))
   }
  
-   
+  if(resData.length === 0 ){
+    return <Shimmer/>
+  } 
+
   return (
     <div className='body'>
       <div className='search'>
---
+++ src/components/Shimmer.js
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Shimmer = () => {
+  return (
+    <div className='shimmerCardConainer'>
+        <div className='shimmerCard'></div>
+        <div className='shimmerCard'></div>
+        <div className='shimmerCard'></div>
+        <div className='shimmerCard'></div>
+        <div className='shimmerCard'></div>
+        <div className='shimmerCard'></div>
+        <div className='shimmerCard'></div>
+        <div className='shimmerCard'></div>
+        <div className='shimmerCard'></div>
+        <div className='shimmerCard'></div>
+    </div>
+    
+  )
+}
+
+export default Shimmer
