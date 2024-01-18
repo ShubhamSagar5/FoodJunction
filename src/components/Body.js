9e790ad9e97462974dd183001b0c5c9c3f94a36e
shubham <shubhamsagar628@gmail.com>
2024-01-18 10:45:30 +0530

    Implement Search Functionality and Top Reated , All Restaurants functionality

--- src/components/Body.js
+++ src/components/Body.js
@@ -5,38 +5,57 @@ import Shimmer from './Shimmer'
 
 const Body = () => {
   
-  const [resData ,setResData] = useState([])
+  const [resData ,setResData] = useState(restaurantsInfo)
   
-  const fetchData = async () => {
-    const ResDataAPI = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=19.9974533&lng=73.78980229999999")
-    const data = await ResDataAPI.json()
-    setResData(data.data.success.cards[4].gridWidget.gridElements.infoWithStyle.restaurants      )
-    // setResData(data?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle?.restaurants)
-  }
+  const [filterRes,setFilterRes] = useState(restaurantsInfo)
 
-useEffect(()=>{
-  fetchData()
-},[])
+  const [searchText,setSearchText] = useState('')
+  // const fetchData = async () => {
+  //   const ResDataAPI = await fetch("https://www.swiggy.com/mapi/homepage/getCards?lat=19.9974533&lng=73.78980229999999")
+  //   const data = await ResDataAPI.json()
+  //   setResData(data.data.success.cards[4].gridWidget.gridElements.infoWithStyle.restaurants      )
+  //   // setResData(data?.data?.success?.cards[3]?.gridWidget?.gridElements?.infoWithStyle?.restaurants)
+  // }
+
+// useEffect(()=>{
+//   fetchData()
+// },[])
   
 
 
   const handleTopRated = () => {
-    setResData(resData.filter((res)=> res.info.avgRating > 4.5))
+    setFilterRes(resData.filter((res)=> res.info.avgRating > 4.5))
   }
  
+  const handleSeacrhFun = () => {
+    const filterResList = resData.filter((res)=> res.info.name.toLocaleLowerCase().includes(searchText.toLocaleLowerCase()))
+    setFilterRes(filterResList)
+  }
+
+
+
   if(resData.length === 0 ){
     return <Shimmer/>
   } 
 
   return (
     <div className='body'>
-      <div className='search'>
-        <h2>Search</h2>
-        <button onClick={handleTopRated}>Top reated Hotel</button>
+      <div className='functionality'>
+        <div className='searchContainer'>
+            <input type="text" value={searchText} onChange={(e)=>{setSearchText(e.target.value)}}/>
+            <button onClick={handleSeacrhFun}>Search</button>
+        </div>
+        <div className='topRatedBtn'>
+          <button onClick={()=>{setFilterRes(restaurantsInfo)}}>All Restaurants</button>
+        </div>
+        <div className='topRatedBtn'>
+                  <button onClick={handleTopRated}>Top reated Hotel</button>
+
+        </div>
       </div>
       <div className='restaurantContainer'>
         {
-        resData?.map((res)=>(<RestaurantCard key={res.info.id} resData={res}/>))
+        filterRes?.map((res)=>(<RestaurantCard key={res.info.id} resData={res}/>))
         }
    
         
