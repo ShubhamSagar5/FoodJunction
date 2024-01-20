deabbd7d10c2e78935df6a5e0155bd35c7c1388e
shubham <shubhamsagar628@gmail.com>
2024-01-20 14:31:08 +0530

    Implement Component Life Cycle Method

--- src/components/About.js
+++ src/components/About.js
@@ -5,8 +5,8 @@ const About = () => {
     return (
         <div>
             <h2>This is about page</h2>
-            <User name={"shubham"} location={"Nashik"+"functionBased"}/>
-            <UserClass name={"shubham"} location={"Nashik"}/>
+           
+            <UserClass />
         </div>
     )
 }
--- src/components/UserClass.js
+++ src/components/UserClass.js
@@ -1,23 +1,106 @@
-import React from "react"
-class UserClass extends React.Component{
+import React from "react";
 
-    constructor(props){
-        super(props)
+class UserClass extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+        UserInfo: {
+
+        }
     }
 
-    render (){
-        
-        const {name,location} = this.props
-        
-        return (
-            <div className="user-card">
-            <h2>Name:{name}</h2>
-            <h3>Location:{location}</h3>
-            <h4>Contact: @shubhamSagar108</h4>
-        </div>
-        )
-    }
+  }
 
+
+  async componentDidMount(){
+    const data = await fetch("https://api.github.com/users/akshaymarch7")
+    const result = await data.json()
+
+    this.setState({
+        UserInfo:result
+    })
+    console.log(this.state.UserInfo)
+}
+
+  
+
+  render() {
+   
+    const {avatar_url, location,name,url} = this.state.UserInfo
+   
+   return <div>
+   <img src={avatar_url} alt="profile img" />
+        <h2>Name : {name}</h2>
+        <h3>Location: {location}</h3>
+        <p>Github Url: <a href={url}>{url}</a></p>
+    </div>;
+  }
 }
 
 export default UserClass
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// import React from "react"
+// class UserClass extends React.Component{
+
+//     constructor(props){
+//         super(props)
+
+//         this.state = {
+//             count:0
+//         }
+
+//     }
+
+//     render (){
+
+//         const {name,location} = this.props
+//         const {count} = this.state
+
+//         return (
+//             <div className="user-card">
+//             <p>{count}</p>
+//             <button onClick={()=>{
+//                 this.setState({
+//                     count:count+1
+//                 })
+//             }}>Inc</button>
+//             <h2>Name:{name}</h2>
+//             <h3>Location:{location}</h3>
+//             <h4>Contact: @shubhamSagar108</h4>
+//         </div>
+//         )
+//     }
+
+// }
+
+// export default UserClass
